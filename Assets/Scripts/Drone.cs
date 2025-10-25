bb73f4c8f7ae5d1971847e48a2634e6f29cf2f18
jGrzyb <grzybjaku@gmail.com>
2025-10-25 20:25:53 +0200

    clean

--- Assets/Scripts/Drone.cs
+++ Assets/Scripts/Drone.cs
@@ -1,35 +1,38 @@
-using System;
-using UnityEditor.Rendering.Universal;
+using System.Linq;
 using UnityEngine;
-using UnityEngine.Animations;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.LowLevel;
 
-public class Drone : MonoBehaviour
-{
-    [SerializeField] Vector3 flMotor = new(-0.5f, 0.0f, 0.5f);
-    [SerializeField] Vector3 frMotor = new(0.5f, 0.0f, 0.5f);
-    [SerializeField] Vector3 blMotor = new(-0.5f, 0.0f, -0.5f);
-    [SerializeField] Vector3 brMotor = new(0.5f, 0.0f, -0.5f);
-    [SerializeField] float gravity = 9.81f;
+public class Drone : MonoBehaviour {
     [SerializeField] float angleMult = 10.0f;
 
-    Rigidbody rb;
-    Vector2 leftJoystick; 
-    Vector2 rightJoystick;
 
-    void Awake()
-    {
+    private Vector2 leftJoystick;
+    private Vector2 rightJoystick;
+
+    private readonly float gravity = Physics.gravity.magnitude;
+    private float mass = 0f;
+
+    private Rigidbody rb;
+
+    private Rotor[] rotors;
+
+
+    void Awake() {
         rb = GetComponent<Rigidbody>();
+        rotors = GetComponentsInChildren<Rotor>();
+        mass = rb.mass + rotors.Sum(x => x.mass);
     }
 
-    void FixedUpdate()
-    {
-        float force = gravity / (4 * Vector3.Dot(transform.up, Vector3.up));
+    void FixedUpdate() {
 
-        Vector3 desiredDir = transform.InverseTransformDirection(-rb.linearVelocity) + new Vector3(rightJoystick.x, 0f, rightJoystick.y) * 4;`
+        var droneRotationY = Quaternion.Euler(0, transform.eulerAngles.y, 0);
+        Vector3 desiredDir = Quaternion.Inverse(droneRotationY) * -rb.linearVelocity + new Vector3(rightJoystick.x, leftJoystick.y, rightJoystick.y) * 4;
         float zRad = (transform.eulerAngles.z + desiredDir.x * angleMult) * Mathf.Deg2Rad;
         float xRad = (transform.eulerAngles.x - desiredDir.z * angleMult) * Mathf.Deg2Rad;
+
+        float deviation = Vector3.Dot(transform.up, Vector3.up);
+        float thrustMultiplier = 1f + desiredDir.y;
+        float force = mass * gravity * thrustMultiplier / 4f;
 
         float flf = force * (1.0f + Mathf.Sin(zRad)) * (1.0f + Mathf.Sin(xRad));
         float frf = force * (1.0f - Mathf.Sin(zRad)) * (1.0f + Mathf.Sin(xRad));
@@ -37,25 +40,23 @@ public class Drone : MonoBehaviour
         float brf = force * (1.0f - Mathf.Sin(zRad)) * (1.0f - Mathf.Sin(xRad));
 
 
-        rb.AddForceAtPosition(transform.up * flf, transform.TransformPoint(flMotor));
-        rb.AddForceAtPosition(transform.up * frf, transform.TransformPoint(frMotor));
-        rb.AddForceAtPosition(transform.up * blf, transform.TransformPoint(blMotor));
-        rb.AddForceAtPosition(transform.up * brf, transform.TransformPoint(brMotor));
+        // rb.AddForceAtPosition(transform.up * flf, transform.TransformPoint(flMotor));
+        // rb.AddForceAtPosition(transform.up * frf, transform.TransformPoint(frMotor));
+        // rb.AddForceAtPosition(transform.up * blf, transform.TransformPoint(blMotor));
+        // rb.AddForceAtPosition(transform.up * brf, transform.TransformPoint(brMotor));
 
-        Debug.DrawLine(transform.position, transform.position + transform.TransformDirection(desiredDir), Color.red);
-        Debug.DrawLine(transform.TransformPoint(flMotor), transform.TransformPoint(flMotor) + transform.up * flf);
-        Debug.DrawLine(transform.TransformPoint(frMotor), transform.TransformPoint(frMotor) + transform.up * frf);
-        Debug.DrawLine(transform.TransformPoint(blMotor), transform.TransformPoint(blMotor) + transform.up * blf);
-        Debug.DrawLine(transform.TransformPoint(brMotor), transform.TransformPoint(brMotor) + transform.up * brf);
+        // Debug.DrawLine(transform.position, transform.position + droneRotationY * desiredDir, Color.red);
+        // Debug.DrawLine(transform.TransformPoint(flMotor), transform.TransformPoint(flMotor) + transform.up * flf);
+        // Debug.DrawLine(transform.TransformPoint(frMotor), transform.TransformPoint(frMotor) + transform.up * frf);
+        // Debug.DrawLine(transform.TransformPoint(blMotor), transform.TransformPoint(blMotor) + transform.up * blf);
+        // Debug.DrawLine(transform.TransformPoint(brMotor), transform.TransformPoint(brMotor) + transform.up * brf);
     }
 
-    public void Move(InputAction.CallbackContext context)
-    {
+    public void Move(InputAction.CallbackContext context) {
         leftJoystick = context.ReadValue<Vector2>();
     }
 
-    public void Look(InputAction.CallbackContext context)
-    {
+    public void Look(InputAction.CallbackContext context) {
         rightJoystick = context.ReadValue<Vector2>();
     }
 }
---
+++ Assets/Scripts/Rotor.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Rotor : MonoBehaviour
+{
+    private Rigidbody rb;
+    private HingeJoint hinge;
+
+    public float mass {
+        get {
+            return rb.mass;
+        }
+    }
+    public Vector3 position {
+        get {
+            return transform.localPosition;
+        }
+    }
+    public float speed {
+        get {
+            return hinge.velocity;
+        }
+    }
+    
+    void Awake() {
+        rb = GetComponent<Rigidbody>();
+        hinge = GetComponent<HingeJoint>();
+    }
+}

c44bc7cdca7d5b40daf24ecfd8633d2c41e804ab
jGrzyb <grzybjaku@gmail.com>
2025-10-26 13:36:06 +0100

    horizontal flight

--- Assets/Scripts/Drone.cs
+++ Assets/Scripts/Drone.cs
@@ -4,59 +4,65 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Drone : MonoBehaviour {
-    [SerializeField] float angleMult = 10.0f;
+    [SerializeField] private float maxTiltAngle = 20f;
+    [SerializeField] private float kp = 1f;
+    [SerializeField] private float kd = 1f;
+    [SerializeField] private float ki = 1f;
+    [SerializeField] private float thrustMultiplier = 1f;
+    [SerializeField] private float dragMultiplier = 1f;
+    [SerializeField] private float rotorDistance = 1f;
+    [SerializeField] private Vector3 fRotorPos = new Vector3(0, 0, 1);
+    [SerializeField] private Vector3 rRotorPos = new Vector3(1, 0, 0);
+    [SerializeField] private Vector3 bRotorPos = new Vector3(0, 0, -1);
+    [SerializeField] private Vector3 lRotorPos = new Vector3(-1, 0, 0);
 
+    private float rollAngle { get { return (transform.eulerAngles.z + 180f) % 360f - 180f; } }
+    private float pitchAngle { get { return (transform.eulerAngles.x + 180f) % 360f - 180f; } }
+    private float yawAngle { get { return (transform.eulerAngles.y + 180f) % 360f - 180f; } }
+
+    private float desiredRoll { get { return -rightJoystick.x * maxTiltAngle * Mathf.Deg2Rad; } }
+    private float desiredPitch { get { return rightJoystick.y * maxTiltAngle * Mathf.Deg2Rad; } }
+    private float desiredYaw { get { return leftJoystick.x * maxTiltAngle * Mathf.Deg2Rad; } }
+
+    private float currentRoll { get { return rollAngle * Mathf.Deg2Rad; } }
+    private float currentPitch { get { return pitchAngle * Mathf.Deg2Rad; } }
+    private float currentYaw { get { return yawAngle * Mathf.Deg2Rad; } }
+
+    private float rollRate { get { return rb.angularVelocity.z; } }
+    private float pitchRate { get { return rb.angularVelocity.x; } }
+    private float yawRate { get { return rb.angularVelocity.y; } }
+
+    private float upwardForce { get { return mass * Physics.gravity.magnitude; } }
+
+    private float mass { get { return rb?.mass ?? 0f; } }
 
     private Vector2 leftJoystick;
     private Vector2 rightJoystick;
-
-    // private readonly float gravity = Physics.gravity.magnitude;
-    private float mass = 0f;
 
     private Rigidbody rb;
 
 
     void Awake() {
         rb = GetComponent<Rigidbody>();
-        mass = rb.mass;
     }
 
     void FixedUpdate() {
-        Vector3 fRotorPos = new Vector3(0, 0, 1);
-        Vector3 rRotorPos = new Vector3(1, 0, 0);
-        Vector3 bRotorPos = new Vector3(0, 0, -1);
-        Vector3 lRotorPos = new Vector3(-1, 0, 0);
-        float kp = 1f;
-        float kd = 1f;
-        float ki = 0f;
-
-        float thrustMultiplier = 1f;
-        float dragMultiplier = 1f;
-
-        float rotorDistance = fRotorPos.magnitude;
-
-
-        float zAngle = (transform.eulerAngles.z + 180f) % 360f - 180f;
-        float desiredRollRad = rightJoystick.x * 20f * Mathf.Deg2Rad;
-        float currentRollRad = zAngle * Mathf.Deg2Rad;
-        float rollRateRad = rb.angularVelocity.x;
-
-        float rollControl = kp * (desiredRollRad - currentRollRad) - kd * rollRateRad;
-
-
-
         Matrix4x4 controlToRotor = new Matrix4x4(
             new Vector4(1, 1, 1, 1),
             new Vector4(0, rotorDistance, 0, -rotorDistance),
-            new Vector4(rotorDistance, 0, -rotorDistance, 0),
+            new Vector4(-rotorDistance, 0, rotorDistance, 0),
             new Vector4(dragMultiplier, -dragMultiplier, dragMultiplier, -dragMultiplier)
-        ).transpose;
+        ).transpose.inverse;
 
-        float upwardForce = mass * 9.81f;
+        float rollControl = kp * (desiredRoll - currentRoll) - kd * rollRate;
+        float pitchControl = kp * (desiredPitch - currentPitch) - kd * pitchRate;
+        float yawControl = kp * (desiredYaw - currentYaw) - kd * yawRate;
 
-        Vector4 controlInput = new(upwardForce, rollControl, 0f, 0f);
+        Debug.Log($"{rb.angularVelocity.z}    {rollRate}    {rollControl}");
 
-        Vector4 rotorForces = controlToRotor.inverse * controlInput;
+
+        Vector4 controlInput = new(upwardForce, rollControl, pitchControl, 0f);
+        Vector4 rotorForces = controlToRotor * controlInput;
 
         rb.AddForceAtPosition(transform.up * rotorForces.x, transform.TransformPoint(fRotorPos));
         rb.AddForceAtPosition(transform.up * rotorForces.y, transform.TransformPoint(rRotorPos));
